557d85957136f99d51500da54333744fb84d4f34
srxvna <143320638+srxvna@users.noreply.github.com>
2024-01-28 14:04:10 +0530

    Add files via upload

---
+++ Dev-Saravana/script.js
@@ -0,0 +1,71 @@
+var menuBtn = document.querySelector('.main-navbar .menu-btn');
+var menuList = document.querySelector('.main-navbar .nav-list');
+var menuListItems = document.querySelectorAll('.nav-list li a');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menuList.classList.toggle('active');
+});
+
+for(var i = 0; i < menuListItems.length; i++){
+	menuListItems[i].addEventListener('click', menuItemClicked);
+}
+function menuItemClicked(){
+	menuBtn.classList.remove('active');
+	menuList.classList.remove('active');
+}
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 120){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+
+$('.partners-slider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+        	items:5
+        }
+    }
+})
+
+$('.testimonials-slider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:6000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        }
+    }
+})
